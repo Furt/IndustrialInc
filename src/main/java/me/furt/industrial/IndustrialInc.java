0429f07457d50d76bb91321ab530d18c70774c49
Furt <lit_69@hotmail.com>
2013-05-26 03:37:07 -0500

    Added test images and first test block.
    Signed-off-by: Furt <lit_69@hotmail.com>

--- src/main/java/me/furt/industrial/IndustrialInc.java
+++ src/main/java/me/furt/industrial/IndustrialInc.java
@@ -1,7 +1,23 @@
 package me.furt.industrial;
 
+import java.util.logging.Level;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IndustrialInc extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		new CustomBlocks(this).init();
+		PluginDescriptionFile pdf = this.getDescription();
+		this.getLogger().log(Level.INFO,
+				"v" + pdf.getVersion() + " is now enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		this.getLogger().log(Level.INFO, "Disabled");
+	}
 
 }
---
+++ src/main/java/me/furt/industrial/block/design/DesignMiningMachine.java
@@ -0,0 +1,14 @@
+package me.furt.industrial.block.design;
+
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.block.design.GenericCubeBlockDesign;
+import org.getspout.spoutapi.block.design.Texture;
+
+public class DesignMiningMachine extends GenericCubeBlockDesign {
+
+	public DesignMiningMachine(Plugin plugin, Texture texture, int[] textureId) {
+		super(plugin, texture, textureId);
+		// TODO Auto-generated constructor stub
+	}
+
+}
