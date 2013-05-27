a8374a43674d45a4eb026d595e3339b4827e1fcb
Furt <lit_69@hotmail.com>
2013-05-27 03:57:35 -0500

    First attempt at making a custom design for Mining Tubes.
    Signed-off-by: Furt <lit_69@hotmail.com>

--- src/main/java/me/furt/industrial/block/BlockMiningTube.java
+++ src/main/java/me/furt/industrial/block/BlockMiningTube.java
@@ -1,17 +1,14 @@
 package me.furt.industrial.block;
 
 import me.furt.industrial.IndustrialInc;
+import me.furt.industrial.block.design.DesignTube;
 
 import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
 
 public class BlockMiningTube extends GenericCubeCustomBlock {
 
 	public BlockMiningTube(IndustrialInc plugin) {
-		super(
-				plugin,
-				"Mining Tube",
-				"https://dl.dropboxusercontent.com/u/17392489/IndustrialInc/miningtube.png",
-				16);
+		super(plugin, "Mining Tube", new DesignTube(plugin));
 		this.setHardness(4.0F);
 	}
 }
