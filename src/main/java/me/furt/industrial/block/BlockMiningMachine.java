558b8f802a217913bac40d67bb92be5d004fe4fa
Furt <lit_69@hotmail.com>
2013-05-28 10:35:11 -0500

    Test #2
    Signed-off-by: Furt <lit_69@hotmail.com>

--- src/main/java/me/furt/industrial/block/BlockMiningMachine.java
+++ src/main/java/me/furt/industrial/block/BlockMiningMachine.java
@@ -8,8 +8,8 @@ public class BlockMiningMachine extends GenericCubeCustomBlock {
 
 	public BlockMiningMachine(IndustrialInc plugin) {
 		super(plugin, "Mining Machine", new DesignGenericBlock(plugin,
-				plugin.assets.getTexture("block_machine"), new int[] { 102, 86,
-						70, 55, 39, 23 }));
+				plugin.assets.getTexture("block_machine"), new int[] { 103, 40,
+						40, 40, 40, 24 }));
 		this.setHardness(4.0F);
 	}
 }
