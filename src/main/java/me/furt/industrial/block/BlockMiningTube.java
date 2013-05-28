ed3b40f9554115904af7e80d44baac4b248c32bb
Furt <lit_69@hotmail.com>
2013-05-28 00:08:31 -0500

    Forgot to modify the block itself.
    Signed-off-by: Furt <lit_69@hotmail.com>

--- src/main/java/me/furt/industrial/block/BlockMiningTube.java
+++ src/main/java/me/furt/industrial/block/BlockMiningTube.java
@@ -3,9 +3,9 @@ package me.furt.industrial.block;
 import me.furt.industrial.IndustrialInc;
 import me.furt.industrial.block.design.DesignTube;
 
-import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
+import org.getspout.spoutapi.material.block.GenericCuboidCustomBlock;
 
-public class BlockMiningTube extends GenericCubeCustomBlock {
+public class BlockMiningTube extends GenericCuboidCustomBlock {
 
 	public BlockMiningTube(IndustrialInc plugin) {
 		super(plugin, "Mining Tube", new DesignTube(plugin));
